d08c80d554726a4750caa0d137c8977b4b4e570d
iklaruri <iklaruri@gmail.com>
2020-07-22 16:16:30 +0200

    CORS aplicado

--- routes/usuario.js
+++ routes/usuario.js
@@ -45,7 +45,7 @@ app.get('/',(req,res,next) => {
 // Crear usuario
 // ======================================
 
-app.post('/', mdAutenticacion.verificarToken, (req,res) => {
+app.post('/', (req,res) => {
   var body = req.body;
 
   var usuario = new Usuario({
